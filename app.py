8dc2474230ec1138f77657f8b95aed5d503a3679
SIddharth Mathur <116292769+SiddharthMathur01@users.noreply.github.com>
2026-05-05 06:55:03 +0530

    production config

--- app.py
+++ app.py
@@ -505,4 +505,4 @@ if __name__ == '__main__':
     print("\nPress CTRL+C to stop the server")
     print("="*70 + "\n")
     
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
